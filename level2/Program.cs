9eae235ca4a247ba32dcab0f2e3029d7b585d4c0
Michael Morf <michael.morf@kcc.at>
2022-11-16 23:07:42 +0100

    level2

---
+++ level2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+string[] input = File.ReadAllLines("level2.in");
+
+Console.WriteLine(Part1(input));
+Console.WriteLine(Part2(input));
+
+
+int Part1(string[] input)
+{
+    //2*l*w + 2*w*h + 2*h*l
+    int sum = input.Select(t => t.Split("x").Select(int.Parse).ToList()).Select(t => (w: t[0], l: t[1], h: t[2], smallest: t.OrderBy(x => x).Take(2).ToArray())).Sum(x => 2*x.l*x.w + 2*x.w*x.h + 2*x.h*x.l + x.smallest[0] * x.smallest[1] );
+    return sum;
+}
+
+int Part2(string[] input)
+{
+    int sum = input.Select(t => t.Split("x").Select(int.Parse).ToList()).Select(t => (w: t[0], l: t[1], h: t[2], smallest: t.OrderBy(x => x).Take(2).ToArray())).Sum(x => 2*x.smallest[0] + 2 * x.smallest[1] + x.l*x.w*x.h);
+    return sum;
+}
