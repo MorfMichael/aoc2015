d0ddcf03641166b0c413219e7177573a519994c6
Ing. Morf Michael <michael.morf@gmx.at>
2022-11-18 16:47:42 +0100

    level9 start

---
+++ level9/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] input = File.ReadAllLines("sample.in");
+
+Console.WriteLine(" --- LEVEL 9 --- ");
+
+Console.WriteLine($"Part1: {Part1(input)}");
+Console.WriteLine($"Part2: {Part2(input)}");
+
+int Part1(string[] input)
+{
+    List<(string Start, string End, int Distance)> result = new List<(string Start, string End, int Distance)>();
+
+    foreach (var line in input)
+    {
+        var split = line.Split(" = ");
+        var split2 = split[0].Split(" to ");
+
+        int distance = int.Parse(split[1]);
+        string source = split2[0];
+        string destination = split2[1];
+        result.Add((source, destination, distance));
+    }
+
+    List<string> destinations = result.SelectMany(t => new[] { t.Start, t.End }).Distinct().ToList();
+
+    Dictionary<string, (string, int)> dict = new Dictionary<string, (string, int)>();
+
+
+
+    return 0;
+}
+
+int Part2(string[] input)
+{
+    return 0;
+}
