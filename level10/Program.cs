623a3cde41954033549f383dd42f92968d6963d9
Michael Morf <michael.morf@kcc.at>
2022-11-21 08:00:28 +0100

    level10 part2

--- level10/Program.cs
+++ level10/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System.IO.Pipes;
+using System.Text;
 
 string input = "3113322113";
 
-//Console.WriteLine("Part1: " + Part1(input, 40));
-Console.WriteLine("Part2: " + Part2(input, 40));
+Console.WriteLine("Part1: " + Part1(input, 40));
+Console.WriteLine("Part2: " + Part1(input, 50));
 
 int Part1(string input, int count)
 {
@@ -13,39 +14,21 @@ int Part1(string input, int count)
         Console.WriteLine(input.Length);
         int c = 0;
         char p = input[0];
-        string tmp = string.Empty;
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < input.Length; i++)
         {
             if (input[i] != p)
             {
-                tmp += c.ToString() + p.ToString();
+                sb.Append(c.ToString() + p.ToString());
                 c = 0;
             }
             c++;
             p = input[i];
         }
 
-        tmp += c.ToString() + p.ToString();
-        input = tmp;
+        sb.Append(c.ToString() + p.ToString());
+        input = sb.ToString();
     }
-    return input.Length;
-}
-
-int Part2(string input, int count)
-{
-    for (int j = 0; j < count; j++)
-    {
-        Console.WriteLine(j);
-        int c = 0, i = 0;
-        char p = input[0];
-        string tmp = string.Empty;
-        
-        //todo...
-        
-        tmp += c.ToString() + p.ToString();
-        input = tmp;
-    }
-
     return input.Length;
 }
