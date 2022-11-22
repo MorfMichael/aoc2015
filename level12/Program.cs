9686173eaacc2d8b59386f96fab4a4b9af6f60d5
Ing. Morf Michael <michael.morf@gmx.at>
2022-11-22 22:48:53 +0100

    level1 part1 + part2

--- level12/Program.cs
+++ level12/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
+using System.Xml.Schema;
 
 string input = File.ReadAllText("level12.in");
 
@@ -15,30 +14,59 @@ int Part1(string input)
     int sum = 0;
 
     var node = JsonNode.Parse(input);
-    while (node != null)
-    {
-        if (node.GetType() == typeof(JsonArray))
-        {
-            foreach (var entry in node.AsArray())
-            {
-                node = entry;
-            }
-        }
-        else if (node.GetType() == typeof(JsonObject))
-        {
-            var obj = node.AsObject();
-            foreach (var entry in obj)
-            {
-                if (int.TryParse(entry.Key, out var parsed)) sum += parsed; ;
-
-            }
-        }
-    }
-
-    return 0;
+    sum += CheckNode(node);
+    return sum;
 }
 
 int Part2(string input)
 {
-    return 0;
+    int sum = 0;
+
+    var node = JsonNode.Parse(input);
+    sum += CheckNode(node);
+    return sum;
+}
+
+int CheckNode(JsonNode node)
+{
+    int sum = 0;
+    try
+    {
+        int value = node.GetValue<JsonElement>().GetInt32();
+        //Console.WriteLine(value);
+        return value;
+    }
+    catch { }
+
+    if (node.GetType() == typeof(JsonArray))
+    {
+        foreach (var entry in node.AsArray())
+        {
+            sum += CheckNode(entry);
+        }
+    }
+    else if (node.GetType() == typeof(JsonObject))
+    {
+        var obj = node.AsObject();
+
+        if (obj.Any(t => GetValue(t.Value) == "red")) return 0;
+
+        foreach ((string key, JsonNode value) in obj)
+        {
+            if (int.TryParse(key, out var parsed)) sum += parsed;
+            sum += CheckNode(value);
+        }
+    }
+
+    return sum;
+}
+
+string GetValue(JsonNode node)
+{
+    try
+    {
+        return node.GetValue<string>();
+    }
+    catch
+    { return null; }
 }
