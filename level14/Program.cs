2401eb2896507937c6b299064f55f1f32aed0b0a
Ing. Morf Michael <michael.morf@gmx.at>
2022-11-23 01:15:19 +0100

    level14 part2

--- level14/Program.cs
+++ level14/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine(" --- LEVEL 14 --- ");
+﻿using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine(" --- LEVEL 14 --- ");
 
 string[] input = File.ReadAllLines("level14.in");
 //input = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\r\nDancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.".Split(Environment.NewLine);
@@ -7,8 +9,9 @@ List<Comet> comets = new();
 Parse(input);
 
 Console.WriteLine("Part1: " + Part1(2503));
+Console.WriteLine("Part2: " + Part2(2503));
 
-Console.WriteLine(string.Join(Environment.NewLine, comets.Select(t => $"{t.Name}: {t.Distance} {t.Rest}")));
+//Console.WriteLine(string.Join(Environment.NewLine, comets));
 
 void Parse(string[] input)
 {
@@ -22,6 +25,8 @@ void Parse(string[] input)
 
 int Part1(int seconds)
 {
+    comets.ForEach(x => x.Reset());
+
     for (int i = 1; i <= seconds; i++)
     {
         foreach (var comet in comets)
@@ -31,6 +36,23 @@ int Part1(int seconds)
     }
 
     return comets.Max(x => x.Distance);
+}
+
+int Part2(int seconds)
+{
+    comets.ForEach(x => x.Reset());
+    for (int i = 1; i <= seconds; i++)
+    {
+        foreach (var comet in comets)
+        {
+            comet.FlyOrRest();
+        }
+
+        int max = comets.Max(x => x.Distance);
+        comets.Where(t => t.Distance == max).ToList().ForEach(x => x.Points++);
+    }
+
+    return comets.Max(x => x.Points);
 }
 
 class Comet
@@ -83,5 +105,13 @@ class Comet
         Counter--;
     }
 
-    public override string ToString() => $"{Name}: {Distance} {(Rest ? "resting" : "flying")}";
+    public void Reset()
+    {
+        Distance = 0;
+        Points = 0;
+        Counter = SpeedTime;
+        Rest = false;
+    }
+
+    public override string ToString() => $"{Name}: {Distance} {Points} {(Rest ? "resting" : "flying")}";
 }
